5c01180f6b2519e3e977b38a27704a7617e06666
nilsmanc <etereaiberia@gmail.com>
2022-10-22 11:42:03 +0300

    pagination

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -2,11 +2,13 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { isLoadingFish, selectFish } from '../../redux/fish/selectors'
 import styles from './Item.module.scss'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppDispatch } from '../../redux/store'
 import { fetchFish } from '../../redux/fish/asyncActions'
 import { Skeleton } from './Skeleton'
 import { NutrionTable } from '../NutrionTable'
+import { Pagination } from '../Pagination'
+import { Button } from '@mui/material'
 
 export const Item: React.FC = () => {
   const navigate = useNavigate()
@@ -29,31 +31,54 @@ export const Item: React.FC = () => {
   useEffect(() => {
     getFish()
   }, [])
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [fishPerPage] = useState(6)
+
+  const lastFishIndex = currentPage * fishPerPage
+  const firstFishIndex = lastFishIndex - fishPerPage
+  const currentFish = data.slice(firstFishIndex, lastFishIndex)
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+  const nextPage = () => setCurrentPage((prev) => prev + 1)
+  const prevPage = () => setCurrentPage((prev) => prev - 1)
+
   return (
     <div>
       {loading == 'loading'
         ? skeletons
-        : data
-            .filter(
-              (block: any) => block['Image Gallery'] && block['Species Name'] !== 'Sugar Kelp',
-            )
-            .map((item: any) => {
-              return (
-                <div
-                  onClick={() => onHandleClick(item)}
-                  className={styles.wrapper}
-                  key={item['Biology']}>
-                  <img className={styles.image} src={item['Species Illustration Photo']?.src} />
-                  <div className={styles.name}>
-                    <span>{item['Species Name']}</span>
-                  </div>
-
-                  <div className={styles.table}>
-                    <NutrionTable fish={item} />
-                  </div>
+        : currentFish.map((item: any) => {
+            return (
+              <div
+                onClick={() => onHandleClick(item)}
+                className={styles.wrapper}
+                key={item['Biology']}>
+                <img className={styles.image} src={item['Species Illustration Photo']?.src} />
+                <div className={styles.name}>
+                  <span>{item['Species Name']}</span>
                 </div>
-              )
-            })}
+
+                <div className={styles.table}>
+                  <NutrionTable fish={item} />
+                </div>
+              </div>
+            )
+          })}
+      <div className={styles.paginator}>
+        {currentPage !== 1 && (
+          <Button variant='contained' onClick={prevPage}>
+            Prev Page
+          </Button>
+        )}
+        <div className={styles.pages}>
+          <Pagination fishPerPage={fishPerPage} totalFish={data.length} paginate={paginate} />
+        </div>
+        {currentPage !== 20 && (
+          <Button variant='contained' onClick={nextPage}>
+            Next Page
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@mui/material'
+
+type PaginationProps = {
+  fishPerPage: number
+  totalFish: number
+  paginate: (number: number) => void
+}
+
+export const Pagination: React.FC<PaginationProps> = ({ fishPerPage, totalFish, paginate }) => {
+  const pageNumbers = []
+
+  for (let i = 1; i <= Math.ceil(totalFish / fishPerPage); i++) {
+    pageNumbers.push(i)
+  }
+  return (
+    <div>
+      {pageNumbers.map((number) => (
+        <Button key={number} variant='contained' onClick={() => paginate(number)}>
+          {number}
+        </Button>
+      ))}
+    </div>
+  )
+}
